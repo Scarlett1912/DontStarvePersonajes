1eda2d304bbafc0a67b3d26632e0819748b21822
Scarlett1915 <digatacat@gmail.com>
2019-06-05 16:06:43 +0200

    Final

---
+++ Beta Don't Starve/BDD_DtS.js
@@ -0,0 +1,215 @@
+var data =
+{
+    "Personajes":[
+        {
+
+            "Don't_Starve": [
+                {
+                    "id":"1",
+                    "Nombre": "Wilson",
+                    "Habilidades": "Le crece una magnífica barba.",
+                    "Requisitos": "Siempre disponible.",
+                    "Cordura": "200",
+                    "Vida": "150",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"2",
+                    "Nombre": "Willow",
+                    "Habilidades": "Inmune al daño del fuego. Tiene un dulce encendedor. Causa incendios cuando está nerviosa.",
+                    "Requisitos": "160 exp (8 días).",
+                    "Cordura": "120",
+                    "Vida": "150",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"3",
+                    "Nombre": "Wolfgang",
+                    "Habilidades": "Se hace más fuerte con la barriga llena. Tiene miedo a la oscuridad y los monstruos.",
+                    "Requisitos": "320 exp (16 días).",
+                    "Cordura": "200",
+                    "Vida": "150-300",
+                    "Hambre": "300"
+                },
+                {
+                    "id":"4",
+                    "Nombre": "Wendy",
+                    "Habilidades": "'Maldita' por su hermana gemela. Cómoda con la oscuridad. No golpea muy fuerte.",
+                    "Requisitos": "640 exp (32 días).",
+                    "Cordura": "200",
+                    "Vida": "150",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"5",
+                    "Nombre": "Wickerbottom",
+                    "Habilidades": "Muy exigente con la comida. Empieza con un nivel técnico gratuito. Auto-publica libros. No puede dormir.",
+                    "Requisitos": "1280 exp (64 días).",
+                    "Cordura": "250",
+                    "Vida": "150",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"6",
+                    "Nombre": "Woodie",
+                    "Habilidades": "Tiene una bonita hacha y un terrible secreto",
+                    "Requisitos": "1600 exp (80 días).",
+                    "Cordura": "200",
+                    "Vida": "150",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"7",
+                    "Nombre": "Wes",
+                    "Habilidades": "No puede hablar, le cuesta mantenerse vivo, practica globoflexia.",
+                    "Requisitos": "Encontrar y rescatar a Locked Wes en el Modo Aventura.",
+                    "Cordura": "150",
+                    "Vida": "113",
+                    "Hambre": "113"
+                },
+                {
+                    "id":"8",
+                    "Nombre": "Maxwell (antagonista)",
+                    "Habilidades": "Es apuesto pero frágil. Puede fragmentar su mente. Lleva su propia Espada oscura.",
+                    "Requisitos": "Completa el Modo Aventura.",
+                    "Cordura": "200",
+                    "Vida": "75",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"9",
+                    "Nombre": "Wagstaff",
+                    "Habilidades": "Es un constructor cualificado. Obtiene un golpe libre de la oscuridad. Trae sus propias herramientas.",
+                    "Requisitos": "Siempre disponible.",
+                    "Cordura": "150",
+                    "Vida": "150",
+                    "Hambre": "225"
+                }
+            ]
+        },
+        {
+            "DtS_Together": [
+                {
+                    "id":"1",
+                    "Nombre": "WX-78",
+                    "Habilidades": "No es delicado con la comida. Es cargado por un rayo; sufre daño por la lluvia. Se puede mejorar con los engranajes.",
+                    "Requisitos": "960 exp (48 días).",
+                    "Cordura": "100-300 a 150-300",
+                    "Vida": "100-400 a 150-400",
+                    "Hambre": "100-200 a 150-200"
+                },
+                {
+                    "id":"2",
+                    "Nombre": "Winona",
+                    "Habilidades": "Es una constructora experta. Obtiene un golpe libre de la oscuridad. Sus inventos propios en gadgets.",
+                    "Requisitos": "Siempre disponible.",
+                    "Cordura": "200",
+                    "Vida": "150",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"3",
+                    "Nombre": "Wortox",
+                    "Habilidades": "Es un imp. Puede saltar a través del tiempo y el espacio. No se alimenta con comidas físicas.",
+                    "Requisitos": "Es un imp. Puede saltar a través del tiempo y el espacio. No se alimenta con comidas físicas.",
+                    "Cordura": "150",
+                    "Vida": "200",
+                    "Hambre": "175"
+                }
+            ]
+        },
+        {
+            "DLC_Reign_of_Giant": [
+                {
+                    "id":"1",
+                    "Nombre": "Wigfrid",
+                    "Habilidades": "Sobresaliente en el combate. Gana Poder de sus enemigos caídos. Sólo come carne.",
+                    "Requisitos": "920 exp (96 días con el dlc Reign of Giant activado).",
+                    "Cordura": "120",
+                    "Vida": "200",
+                    "Hambre": "120"
+                },
+                {
+                    "id":"2",
+                    "Nombre": "Webber",
+                    "Habilidades": "Es un monstruo. puede socializar con Arañas. Le crece una sedosa y suave barba.",
+                    "Requisitos": "Encontrar el Cráneo de Webber y enterrarlo dentro de una tumba abierta cuando se juegue con DLC Reign of Giants activado.",
+                    "Cordura": "100",
+                    "Vida": "175",
+                    "Hambre": "175"
+                }
+            ]
+        },
+        {
+            "DCL_Shipwrecked": [
+                {
+                    "id":"1",
+                    "Nombre": "Walani",
+                    "Habilidades": "Ama surfear. Se seca rápidamente. Es una chica bastante fría.",
+                    "Requisitos": "2240 exp (112 días con el dlc Shipwrecked activado).",
+                    "Cordura": "200",
+                    "Vida": "120",
+                    "Hambre": "200"
+                },
+                {
+                    "id":"2",
+                    "Nombre": "Warly",
+                    "Habilidades": "Tiene gustos distinguidos. Cocinero con utensilios de cocina personalizado. Tiene una elegante bolsa de cheff.",
+                    "Requisitos": "2240 exp (112 días con el dlc Shipwrecked activado).",
+                    "Cordura": "200",
+                    "Vida": "150",
+                    "Hambre": "200"
+                },
+                {
+                    "id":"3",
+                    "Nombre": "Wilbur",
+                    "Habilidades": "No puede hablar. Puede correr. Es un mono.",
+                    "Requisitos": "Encontrar la Corona manchada y devolverla a Wilbur cuando se juega en una partida de Shipwrecked.",
+                    "Cordura": "150",
+                    "Vida": "125",
+                    "Hambre": "175"
+                },
+                {
+                    "id":"4",
+                    "Nombre": "Woodlegs",
+                    "Habilidades": "Puede olfatear tesoros. Capitán del 'Piernas de mar'. Es un pirata.",
+                    "Requisitos": "Encuentra la jaula de Woodleg en el volcán y luego liberarlo con las llaves de hierro, hueso y oro mientras juega en una partida de Shipwrecked.",
+                    "Cordura": "120",
+                    "Vida": "150",
+                    "Hambre": "150"
+                }
+            ]
+        },
+        {
+            "DCL_Hamlet": [
+                {
+                    "id":"1",
+                    "Nombre": "Wilba",
+                    "Habilidades": "Es una cerda. Carga con el legado de su padre. Le tiene miedo a la oscuridad.",
+                    "Requisitos": "Consigue la Corona Real por derrotar a la Reina Womant, y darsela a la Reina Cerdo en el Palacio mientras juega en una partida de Hamlet.",
+                    "Cordura": "100",
+                    "Vida": "150",
+                    "Hambre": "200"
+                },
+                {
+                    "id":"2",
+                    "Nombre": "Wormwood",
+                    "Habilidades": "",
+                    "Requisitos": "Siempre disponible.",
+                    "Cordura": "100",
+                    "Vida": "150",
+                    "Hambre": "150"
+                },
+                {
+                    "id":"3",
+                    "Nombre": "Wheeler",
+                    "Habilidades": "Armada con una bocina. Tiene un Navigadget para encontrar cosas. Viaja más ligera que la mayoría.",
+                    "Requisitos": "3200 exp (160 días con el DLC Hamlet activado).",
+                    "Cordura": "200",
+                    "Vida": "150",
+                    "Hambre": "150"
+                }
+            ]
+        }
+    ]
+}
